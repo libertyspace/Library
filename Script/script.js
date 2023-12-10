93e6428a54eaeb9e7f0c586c4eefd53a9df23f39
libertyspace <libertyspace4@gmail.com>
2023-12-10 23:34:02 +0100

    Created and CSSed Buttons, Introduced Modal Dialog, event Listeners for Read and romove. Gave the Prototype of the Books a function to change read status...

--- Script/script.js
+++ Script/script.js
@@ -2,13 +2,120 @@ const myLibrary = [];
 const libraryList = document.querySelector("#library");
 const interaction = document.querySelector("#interaction");
 const btnNewEntry = document.querySelector("#new-entry");
+const dialog = document.querySelector("dialog");
+const readButtons = document.querySelectorAll("buttonReadStatus");
 
-function Book(title, author, genre, description) {
+//Modal elements
+
+// defining Elements
+
+let divForm = document.createElement("div");
+let formForm = document.createElement("form");
+let labelTitle = document.createElement("label");
+let labelAuthor = document.createElement("label");
+let labelDescription = document.createElement("label");
+let labelGenre = document.createElement("label");
+let inputTitle = document.createElement("input");
+let inputAuthor = document.createElement("input");
+let inputDescription = document.createElement("textarea");
+let inputGenre = document.createElement("input");
+let divFormTitle = document.createElement("div");
+let divFormAuthor = document.createElement("div");
+let divFormGenre = document.createElement("div");
+let divFormDescription = document.createElement("div");
+let divFormButtons = document.createElement("div");
+let buttonFormSubmit = document.createElement("button");
+let buttonFormReset = document.createElement("button");
+let buttonFormCancel = document.createElement("button");
+
+// Setting Attributes
+
+formForm.setAttribute("method", "post");
+formForm.setAttribute("action", "./test.html");
+
+labelTitle.setAttribute("for", "title");
+inputTitle.setAttribute("id", "title");
+inputTitle.setAttribute("name", "title");
+inputTitle.setAttribute("type", "text");
+
+labelAuthor.setAttribute("for", "author");
+inputAuthor.setAttribute("id", "author");
+inputAuthor.setAttribute("name", "author");
+inputAuthor.setAttribute("type", "text");
+
+labelGenre.setAttribute("for", "genre");
+inputGenre.setAttribute("id", "genre");
+inputGenre.setAttribute("name", "genre");
+inputGenre.setAttribute("type", "text");
+
+labelDescription.setAttribute("for", "description");
+inputDescription.setAttribute("id", "description");
+inputDescription.setAttribute("name", "description");
+inputDescription.setAttribute("rows", "15");
+
+buttonFormSubmit.setAttribute("type", "submit");
+buttonFormSubmit.setAttribute("id", "button-submit");
+buttonFormReset.setAttribute("type", "reset");
+buttonFormCancel.setAttribute("type", "button");
+
+// Appends
+
+dialog.appendChild(divForm);
+divForm.appendChild(formForm);
+formForm.appendChild(divFormTitle);
+formForm.appendChild(divFormAuthor);
+formForm.appendChild(divFormGenre);
+formForm.appendChild(divFormDescription);
+formForm.appendChild(divFormButtons);
+divFormTitle.appendChild(labelTitle);
+divFormTitle.appendChild(inputTitle);
+divFormAuthor.appendChild(labelAuthor);
+divFormAuthor.appendChild(inputAuthor);
+divFormGenre.appendChild(labelGenre);
+divFormGenre.appendChild(inputGenre);
+divFormDescription.appendChild(labelDescription);
+divFormDescription.appendChild(inputDescription);
+divFormButtons.appendChild(buttonFormSubmit);
+divFormButtons.appendChild(buttonFormReset);
+divFormButtons.appendChild(buttonFormCancel);
+
+// adding text
+
+labelTitle.textContent = "Title: ";
+labelAuthor.textContent = "Author: ";
+labelGenre.textContent = "Genre: ";
+labelDescription.textContent = "Description: ";
+buttonFormSubmit.textContent = "Submit";
+buttonFormReset.textContent = "Reset";
+buttonFormCancel.textContent = "Cancel";
+
+// setting the classes
+divFormTitle.classList.toggle("grid-form-inputs");
+divFormAuthor.classList.toggle("grid-form-inputs");
+divFormGenre.classList.toggle("grid-form-inputs");
+divFormDescription.classList.toggle("grid-form-description");
+divFormButtons.classList.toggle("grid-form-buttons");
+formForm.classList.toggle("grid-form");
+
+// Book Constructor
+
+function Book(title, author, genre, description, readStatus) {
   this.title = title;
   this.author = author;
   this.genre = genre;
   this.description = description;
+  this.readStatus = readStatus;
 }
+
+// Book Prototype
+
+Book.prototype.setReadStatus = function () {
+  if (this.readStatus === "read") {
+    this.readStatus = "unread";
+  } else if (this.readStatus === "unread" || this.readStatus === undefined) {
+    this.readStatus = "read";
+  }
+};
 
 let book1 = new Book(
   "The Hunger Games",
@@ -43,152 +150,83 @@ function addBookToLibraryArray(book) {
 }
 
 function addBookToLibraryDom(book) {
+  // Variables
+
   let divCard = document.createElement("div");
   let divCardLeftSection = document.createElement("div");
   let divCardRightSection = document.createElement("div");
+  let divCardConatinerTitleAuthorGenre = document.createElement("div");
   let divCardTitle = document.createElement("p");
   let divCardAuthor = document.createElement("p");
   let divCardGenre = document.createElement("p");
   let pCardDescription = document.createElement("p");
   let divCardDesciption = document.createElement("div");
   let divCardReadToggle = document.createElement("div");
-  let pCardReadToggle = document.createElement("p");
-  let inputCboxCardReadToggle = document.createElement("input");
 
-  inputCboxCardReadToggle.setAttribute("type", "checkbox");
+  let buttonCardReadStatus = document.createElement("button");
+  let buttonCardRemove = document.createElement("button");
+  // Setting Attributes
+
+  buttonCardReadStatus.setAttribute("class", "buttonReadStatus");
+  buttonCardRemove.setAttribute("class", "buttonCardRemove");
+  // Adding Classes
 
   divCard.classList.toggle("card");
   divCardRightSection.classList.toggle("grid-row");
   divCardRightSection.classList.toggle("padding");
   divCardLeftSection.classList.toggle("divCardLeftSection");
   divCardReadToggle.classList.toggle("read-toggle");
-  inputCboxCardReadToggle.classList.toggle("checkbox");
-  pCardReadToggle.classList.toggle("center");
   divCardDesciption.classList.toggle("overflow-auto");
+
+  // Adding to the DOM
 
   libraryList.appendChild(divCard);
   divCard.appendChild(divCardLeftSection);
   divCard.appendChild(divCardRightSection);
-  divCardRightSection.appendChild(divCardTitle);
-  divCardRightSection.appendChild(divCardAuthor);
-  divCardRightSection.appendChild(divCardGenre);
+  divCardRightSection.appendChild(divCardConatinerTitleAuthorGenre);
+  divCardConatinerTitleAuthorGenre.appendChild(divCardTitle);
+  divCardConatinerTitleAuthorGenre.appendChild(divCardAuthor);
+  divCardConatinerTitleAuthorGenre.appendChild(divCardGenre);
   divCardRightSection.appendChild(divCardDesciption);
   divCardDesciption.appendChild(pCardDescription);
   divCardRightSection.appendChild(divCardReadToggle);
-  divCardReadToggle.appendChild(pCardReadToggle);
-  divCardReadToggle.appendChild(inputCboxCardReadToggle);
+  divCardReadToggle.appendChild(buttonCardReadStatus);
+  divCardReadToggle.appendChild(buttonCardRemove);
+
+  //Adding Text
 
   divCardTitle.textContent = "Title: " + book.title;
   divCardAuthor.textContent = "Author: " + book.author;
   divCardGenre.textContent = "Genre: " + book.genre;
   pCardDescription.textContent = "Descirption: " + book.description;
-  pCardReadToggle.textContent = "Book Read: ";
+  buttonCardReadStatus.textContent = "Read";
+  buttonCardRemove.textContent = "Remove";
+
+  buttonCardRemove.addEventListener("click", function (e) {});
 }
 
 btnNewEntry.addEventListener("click", function (e) {
-  // Removing the Button that is the Event Listener - will be addes again with Submit Reset Button
+  dialog.showModal();
+});
 
-  interaction.removeChild(btnNewEntry);
+buttonFormCancel.addEventListener("click", function (e) {
+  dialog.close();
+});
 
-  // defining Elements
+// submit Button Listener
 
-  let divForm = document.createElement("div");
-  let formForm = document.createElement("form");
-  let labelTitle = document.createElement("label");
-  let labelAuthor = document.createElement("label");
-  let labelDescription = document.createElement("label");
-  let labelGenre = document.createElement("label");
-  let inputTitle = document.createElement("input");
-  let inputAuthor = document.createElement("input");
-  let inputDescription = document.createElement("textarea");
-  let inputGenre = document.createElement("input");
-  let divFormTitle = document.createElement("div");
-  let divFormAuthor = document.createElement("div");
-  let divFormGenre = document.createElement("div");
-  let divFormDescription = document.createElement("div");
-  let divFormButtons = document.createElement("div");
-  let buttonFormSubmit = document.createElement("button");
-  let buttonFormReset = document.createElement("button");
-  let buttonFormCancel = document.createElement("button");
-
-  // Setting Attributes
-
-  formForm.setAttribute("method", "post");
-  formForm.setAttribute("action", "./test.html");
-
-  labelTitle.setAttribute("for", "title");
-  inputTitle.setAttribute("id", "title");
-  inputTitle.setAttribute("name", "title");
-  inputTitle.setAttribute("type", "text");
-
-  labelAuthor.setAttribute("for", "author");
-  inputAuthor.setAttribute("id", "author");
-  inputAuthor.setAttribute("name", "author");
-  inputAuthor.setAttribute("type", "text");
-
-  labelGenre.setAttribute("for", "genre");
-  inputGenre.setAttribute("id", "genre");
-  inputGenre.setAttribute("name", "genre");
-  inputGenre.setAttribute("type", "text");
-
-  labelDescription.setAttribute("for", "description");
-  inputDescription.setAttribute("id", "description");
-  inputDescription.setAttribute("name", "description");
-  inputDescription.setAttribute("rows", "15");
-
-  buttonFormSubmit.setAttribute("type", "submit");
-  buttonFormSubmit.setAttribute("id", "button-submit");
-  buttonFormReset.setAttribute("type", "reset");
-  buttonFormCancel.setAttribute("type", "button");
-
-  // Appends
-
-  interaction.appendChild(divForm);
-  divForm.appendChild(formForm);
-  formForm.appendChild(divFormTitle);
-  formForm.appendChild(divFormAuthor);
-  formForm.appendChild(divFormGenre);
-  formForm.appendChild(divFormDescription);
-  formForm.appendChild(divFormButtons);
-  divFormTitle.appendChild(labelTitle);
-  divFormTitle.appendChild(inputTitle);
-  divFormAuthor.appendChild(labelAuthor);
-  divFormAuthor.appendChild(inputAuthor);
-  divFormGenre.appendChild(labelGenre);
-  divFormGenre.appendChild(inputGenre);
-  divFormDescription.appendChild(labelDescription);
-  divFormDescription.appendChild(inputDescription);
-  divFormButtons.appendChild(buttonFormSubmit);
-  divFormButtons.appendChild(buttonFormReset);
-  divFormButtons.appendChild(buttonFormCancel);
-
-  // adding text
-
-  labelTitle.textContent = "Title: ";
-  labelAuthor.textContent = "Author: ";
-  labelGenre.textContent = "Genre: ";
-  labelDescription.textContent = "Description: ";
-  buttonFormSubmit.textContent = "Submit";
-  buttonFormReset.textContent = "Reset";
-  buttonFormCancel.textContent = "Cancel";
-
-  // setting the classes
-  divFormTitle.classList.toggle("grid-form-inputs");
-  divFormAuthor.classList.toggle("grid-form-inputs");
-  divFormGenre.classList.toggle("grid-form-inputs");
-  divFormDescription.classList.toggle("grid-form-description");
-
-  // submit Button Listener
-  buttonFormSubmit.addEventListener("click", function (e) {
-    e.preventDefault();
-    const newBook = new Book(
-      inputTitle.value,
-      inputAuthor.value,
-      inputGenre.value,
-      inputDescription.value
-    );
-    addBookToLibraryDom(newBook);
-  });
+buttonFormSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  dialog.close();
+  const newBook = new Book(
+    inputTitle.value,
+    inputAuthor.value,
+    inputGenre.value,
+    inputDescription.value
+  );
+  addBookToLibraryDom(newBook);
+  addBookToLibraryArray(newBook);
+  newBook.index = myLibrary.length - 1;
 });
 
 addBookToLibraryArray(book1);
@@ -196,7 +234,42 @@ addBookToLibraryArray(book2);
 addBookToLibraryArray(book3);
 addBookToLibraryArray(book4);
 
+for (let i = 0; i < myLibrary.length; i++) {
+  myLibrary[i].index = i;
+}
 for (book of myLibrary) {
   addBookToLibraryDom(book);
 }
 console.log(myLibrary);
+
+const removeButtons = document.querySelectorAll(".buttonCardRemove");
+
+libraryList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("buttonCardRemove")) {
+    e.preventDefault();
+    const card = e.target.closest(".card");
+    const index = Array.from(libraryList.children).indexOf(card);
+    removeBookFromLibrary(index);
+    libraryList.removeChild(card);
+  }
+});
+
+libraryList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("buttonReadStatus")) {
+    e.preventDefault();
+    const card = e.target.closest(".card");
+    const index = Array.from(libraryList.children).indexOf(card);
+    myLibrary[index].setReadStatus();
+    console.log(myLibrary[index]);
+    card.classList.toggle("card-read");
+    if (e.target.textContent === "Read") {
+      e.target.textContent = "Unread";
+    } else if (e.target.textContent === "Unread") {
+      e.target.textContent = "Read";
+    }
+  }
+});
+
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1);
+}
